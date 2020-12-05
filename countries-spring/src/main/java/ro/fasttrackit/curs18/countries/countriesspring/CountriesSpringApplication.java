376778e8b4d8030407c70cad23f764b5a9fcbbb5
Raul Pincotan <raul.pincotan91@gmail.com>
2020-12-05 15:35:28 +0000

    first spring

--- countries-spring/src/main/java/ro/fasttrackit/curs18/countries/countriesspring/CountriesSpringApplication.java
+++ countries-spring/src/main/java/ro/fasttrackit/curs18/countries/countriesspring/CountriesSpringApplication.java
@@ -8,13 +8,7 @@ public class CountriesSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CountriesSpringApplication.class, args);
-        CountryService countryService = new CountryService();
-        System.out.println(countryService.getCountries());
-        System.out.println(countryService.getCapitalOfCountry(187));
-        System.out.println();
-        System.out.println(countryService.getCountriesWithLargerPopulation("Europe", 20000000));
-        System.out.println();
-        System.out.println(countryService.getCountryWithSelectedNeighbours("HUN", "ROU"));
+
 
     }
 
