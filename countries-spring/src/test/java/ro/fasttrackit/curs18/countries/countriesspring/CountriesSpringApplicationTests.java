a1b891a7ef6f2dda4bed89eca6bb660cfa5ceab6
Raul Pincotan <raul.pincotan91@gmail.com>
2020-12-05 15:35:42 +0000

    first spring

---
+++ countries-spring/src/test/java/ro/fasttrackit/curs18/countries/countriesspring/CountriesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.curs18.countries.countriesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
